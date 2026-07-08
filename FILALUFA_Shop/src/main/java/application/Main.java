57c9905fdaceee8be8a2091f0b00fc5a9020f59f
HYPE AMD <fitrahjuliansyah1207@gmail.com>
2026-07-08 14:09:23 +0700

    UPDATE MAIN

--- FILALUFA_Shop/src/main/java/application/Main.java
+++ FILALUFA_Shop/src/main/java/application/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
         stage.setResizable(true);
         stage.centerOnScreen();
         stage.show();
+        //ABIDIN HABSYI GANTENG SEKALI
 
     }
 
